0873634ecd2d13b1fc034018caaebf03c17169de
Oliver Norred <norredoliver@gmail.com>
2021-07-27 14:06:40 -0700

    Initial commit

---
+++ parallax.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('.parallax').forEach(el => {
+
+	let offsetTop = offset(el).top
+
+	// el.style.position = "relative"
+	let scrollfactor = el.getAttribute('parallax')
+	
+	addEventListener('scroll', ()=> {
+		requestAnimationFrame(()=> {
+			el.style.transform = "translate(0, " + -scrollfactor * window.scrollY + "px)"
+			// el.style.top = -scrollfactor * window.scrollY + "px"
+		})
+	})
+})
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
